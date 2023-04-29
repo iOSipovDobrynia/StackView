5a49a49892b074c86a0b4a02a20663370187901d
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-04-29 13:28:25 +0300

    test

--- StackView/ViewController.swift
+++ StackView/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var redLight: UIView!
+        
+    @IBOutlet var changeLightButton: UIButton!
+    
+    var countOfTap = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        makePaleCircle(from: greenLight)
+        makePaleCircle(from: yellowLight)
+        makePaleCircle(from: redLight)
+        
+        changeLightButton.layer.cornerRadius = 15
     }
 
-
+    private func makePaleCircle(from light: UIView) {
+        light.layer.cornerRadius = light.bounds.width / 2
+        paleCircle(of: light)
+    }
+    
+    private func paleCircle(of light: UIView) {
+        light.alpha = 0.3
+    }
+    
+    @IBAction func buttonPressed() {
+        countOfTap += 1
+        changeLightButton.setTitle("Next", for: .normal)
+        switch countOfTap % 3 {
+        case 1:
+            redLight.alpha = 1
+            paleCircle(of: yellowLight)
+            paleCircle(of: greenLight)
+        case 2:
+            yellowLight.alpha = 1
+            paleCircle(of: redLight)
+            paleCircle(of: greenLight)
+        default:
+            greenLight.alpha = 1
+            paleCircle(of: redLight)
+            paleCircle(of: yellowLight)
+        }
+        
+    }
 }
 
